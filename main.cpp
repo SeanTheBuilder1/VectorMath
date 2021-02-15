2135d1b5201b9cb9c8d02c2761b811bcb52a3928
SeanTheBuilder1 <seanthebuilder52@gmail.com>
2021-02-15 11:26:09 +0800

    minor changes

--- main.cpp
+++ main.cpp
@@ -6,19 +6,20 @@
 
 
 int main(){
-    float a, b, c, d;
-    std::cout << "x^1 = "; 
-    std::cin >> a;
-    std::cout << "y^1 = ";
-    std::cin >> b;
-    std::cout << "x^2 = ";
-    std::cin >> c;
-    std::cout << "y^2 = ";
-    std::cin >> d;
-     
-    glm::vec2 x = glm::vec2(a, b);
-    glm::vec2 y = glm::vec2(c, d);
-    float lmao = glm::distance(x, y);
-    std::cout << lmao;
-    return 0;
+    while(true){
+        float a, b, c, d;
+        std::cout << "x^1 = "; 
+        std::cin >> a;
+        std::cout << "y^1 = ";
+        std::cin >> b;
+        std::cout << "x^2 = ";
+        std::cin >> c;
+        std::cout << "y^2 = ";
+        std::cin >> d;
+            
+        glm::vec2 x = glm::vec2(a, b);
+        glm::vec2 y = glm::vec2(c, d);
+        float lmao = glm::distance(x, y);
+        std::cout << "distance = " << lmao << "\n";
+    }
 }
